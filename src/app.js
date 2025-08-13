c5f688bcdb4d9c370edb815bd8b5345bed3b2959
JoseZL <josezamora485@gmail.com>
2025-08-13 12:20:52 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let pronoun = ['el', 'nuestro'];
+let adj = ['buen', 'gran'];
+let noun = ['mono', 'mapache'];
+let extensions = ['.com', '.net', '.us', '.io'];
+
+let lista = document.getElementById('lista');
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      for (let e of extensions) {
+        let dominio = p + a + n + e;
+        let li = document.createElement('li');
+        li.textContent = dominio;
+        lista.appendChild(li);
+      }
+    }
+  }
+}
+
 };
